2ccfc7232d66fd898c2c5cb4bced506a9b927c6c
Side-Flip <milo_0922@outlook.com>
2024-10-08 16:02:35 -0500

    "Añadidos modelos de Producto, Cliente, Vendedor, Factura, and DetalleFactura; y se establecio la conexion con la base de datos

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    stock = models.IntegerField()
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=20)
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20)
+    usuario = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
+    rol = models.CharField(max_length=50)
+
+class Factura(models.Model):
+    fecha = models.DateField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
+
+class DetalleFactura(models.Model):
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad_producto = models.IntegerField()
+    total = models.FloatField()
