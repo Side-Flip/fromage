8c35f69b32f0a6d725a0327ca5b29deab28a056e
AlejaOM <ortizalejamedina@gmail.com>
2024-10-14 14:07:56 -0500

    fix login and home

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  constructor(private router: Router) {}
 
+  showNavbar(): boolean {
+    return this.router.url !== '/login'; // Oculta en '/login'
+  }
 }
