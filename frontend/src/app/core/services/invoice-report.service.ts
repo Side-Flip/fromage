66143c57ffd9c3d119d1b15133960e677f710717
AlejaOM <ortizalejamedina@gmail.com>
2024-11-14 11:04:09 -0500

    search invoices response wit json ok

--- frontend/src/app/core/services/invoice-report.service.ts
+++ frontend/src/app/core/services/invoice-report.service.ts
@@ -1,24 +1,56 @@
-import { HttpClient } from '@angular/common/http';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceReportService {
-  private API_URL = 'http://localhost:8000/api/data';
+  private API_URL = 'http://localhost:8000/reporte/facturas/';
+  private sucursalesURL = 'http://localhost:8000/reporte/sucursales/';
+
+  constructor(private http: HttpClient) {}
+
+  private getAuthToken(): string | null {
+    if (typeof window !== 'undefined' && localStorage) {
+        return localStorage.getItem('authToken');
+    }
+    return null;
+}
+
+  getData(option: string, startDate: string, endDate: string, sucursal: string): Observable<any> {
+    const token = this.getAuthToken();
+    if (!token) {
+        console.error("Token de autenticación no encontrado. Verifica que el usuario haya iniciado sesión.");
+        return throwError(() => new Error("Token de autenticación no encontrado."));
+    }
+    const params = { option, fecha_inicio: startDate, fecha_fin: endDate, sucursal };
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+    return this.http.get<any>(this.API_URL, { params, headers });
+}
+
+  getSucursales(): Observable<any[]> {
+    const token = this.getAuthToken();
+    if (!token) {
+      console.error("Token de autenticación no encontrado. Verifica que el usuario haya iniciado sesión.");
+      return throwError(() => new Error("Token de autenticación no encontrado."));
+    }
   
-  constructor(private http: HttpClient) { }
-
-  getData(option: string, startDate: string, endDate: string): Observable<any> {
-   
-    const body = {
-      option,
-      start_date: startDate,
-      end_date: endDate
-    };
-
-    
-    return this.http.post<any>(this.API_URL, body);
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+  
+    return this.http.get<any[]>(this.sucursalesURL, { headers });
   }
+
+  getInvoiceDetail(numeroFactura: number): Observable<any> {
+    const token = this.getAuthToken();
+    if (!token) {
+      console.error("Token de autenticación no encontrado. Verifica que el usuario haya iniciado sesión.");
+      return throwError(() => new Error("Token de autenticación no encontrado."));
+    }
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+    return this.http.get<any>(`http://localhost:8000/reporte/factura-detalle/${numeroFactura}/`, { headers });
+  }
+
+  
 }
